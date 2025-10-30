ad25fd9f175d055219f1eca222e3141e93110f41
Saus <didukhirina122@gmail.com>
2025-10-30 12:43:17 +0200

    Add files via upload

---
+++ lab7/bot.py
@@ -0,0 +1,18 @@
+import telebot
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def answer(message):
+    bot.send_message(message.chat.id, text='/start')
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__' :
+    bot.polling()
